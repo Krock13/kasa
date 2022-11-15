0abb2371a3322d236187a571f44da891b4942409
krock13 <krock.airdrop@hotmail.com>
2022-11-15 14:33:00 +0100

    Creation of all routes and corresponding pages

--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,10 @@ import React, { StrictMode } from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/home'
+import Lodging from './pages/Lodging/lodging'
+import About from './pages/About/about'
+import Footer from './components/Footer/footer'
+import Error from './components/Error/error'
 import GlobalStyle from './utils/style/GlobalStyle'
 
 ReactDOM.render(
@@ -10,7 +14,11 @@ ReactDOM.render(
             <GlobalStyle />
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/lodging' element={<Lodging />} />
+                <Route path='/about' element={<About />} />
+                <Route path='*' element={<Error />} />
             </Routes>
+            <Footer />
         </Router>
     </StrictMode>,
     document.getElementById('root'),
