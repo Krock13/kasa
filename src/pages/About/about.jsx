f41d5ba79f426aed0a5c59b5cc9dd69860414151
krock13 <krock.airdrop@hotmail.com>
2022-11-28 15:49:59 +0100

    Adding code for "about" page and multiple rectifications of css

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -1,9 +1,40 @@
+import styles from './about.module.css';
+import banner from '../../assets/banner_about.png';
+import { aboutData } from './aboutData';
+import Collapsible from '../../components/Collapse/collapse';
+
 function About() {
-  return (
-      <div>
-          <h1>About</h1>
-      </div>
-  )
+    return (
+        <div className={styles.aboutContainer}>
+            <div className={styles.bannerContainer}>
+                <img src={banner} alt='' className={styles.banner}></img>
+            </div>
+            <div>
+                {aboutData.map((data, index) => {
+                    return (
+                        <div key={index}>
+                            <Collapsible
+                                title='Fiabilité'
+                                content={data.fiabilite}
+                            />
+                            <Collapsible
+                                title='Respect'
+                                content={data.respect}
+                            />
+                            <Collapsible
+                                title='Service'
+                                content={data.service}
+                            />
+                            <Collapsible
+                                title='Sécurité'
+                                content={data.securite}
+                            />
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
 }
 
-export default About
+export default About;
